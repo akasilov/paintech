e710e0fabf1f913db004e1a351222cbef5a56140
hamzaawais <hamzaawais@gmail.com>
2020-08-13 04:35:29 +0500

    Footer responsive changes

--- src/components/Shared/Footer.js
+++ src/components/Shared/Footer.js
@@ -7,56 +7,71 @@ import * as links from "../../assets/config/links";
 function Footer(props) {
   const { t } = props;
   return (
-    <div id="footer">
-      <div id="logo">
-        <SvgLogo />
-      </div>
-      <ul id="social">
-        <li>
-          <a href={links.instagram}>
-            <i class="fa fa-instagram"></i>
-          </a>
-        </li>
-        <li>
-          <a href={links.facebook}>
-            <i class="fa fa-facebook"></i>
-          </a>
-        </li>
-        <li>
-          <a href={links.youtube}>
-            <i class="fa fa-youtube"></i>
-          </a>
-        </li>
-        <li>
-          <a href={links.telegram}>
-            <i class="fa fa-telegram"></i>
-          </a>
-        </li>
-      </ul>
-      <ul id="nav-menu">
-        <li>
-          <Link to="/about">{t("home_page.footer.nav_menu.about")}</Link>
-        </li>
-        <li>
-          <Link to="/terms">
-            {t("home_page.footer.nav_menu.sales_conditions")}
-          </Link>
-        </li>
-      </ul>
-    <div className="w-100 copyright pull-left pt-4 pb-4">
-          <div className="w-50 pull-left">
-            <p>Email us: info@painted.ch <br/>
+    <footer class="page-footer font-small teal pt-4">
+      <div class="container-fluid text-center text-md-left">
+        <div class="row">
+          <div class="offset-sm-2 offset-xs-1 col-xs-10 col-sm-8">
+            <div class="row">
+            <div class="col-md-4 mt-md-0 mt-3">
+              <div id="logo">
+                <SvgLogo />
+              </div>
+              <p class="contact text-left">Email us: info@painted.ch <br/>
             Schweiz <br/>
             UID: CHE‐326.878.129 </p>
           </div>
+          <hr class="clearfix w-100 d-md-none pb-3"></hr>
+          <div class="col-md-4 mb-md-0 mb-3">
 
-          <div className="w-50 pull-left text-right">
-          <br/>
-          <br/>
-            <p>(C) 2020 www.painted.ch All rights reserved</p>
+              <ul id="nav-menu">
+                <li>
+                  <Link to="/about">
+                    {t("home_page.footer.nav_menu.about")}
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/terms">
+                    {t("home_page.footer.nav_menu.sales_conditions")}
+                  </Link>
+                </li>
+              </ul>
+
           </div>
+          <div class="col-md-4 mb-md-0 mb-3" id="social">
+              <ul >
+                <li>
+                  <a href={links.instagram}>
+                    <i class="fa fa-instagram"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href={links.facebook}>
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href={links.youtube}>
+                    <i class="fa fa-youtube"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href={links.telegram}>
+                    <i class="fa fa-telegram"></i>
+                  </a>
+                </li>
+              </ul>
+      
           </div>
-    </div>
+            </div>
+          </div>
+    
+        </div>
+
+        <div class="footer-copyright text-center py-3">
+          © 2020 www.painted.ch All rights reserved
+        </div>
+      </div>
+    </footer>
   );
 }
 
