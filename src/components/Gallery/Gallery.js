22b089eb50e3af096a7a78fbaf508d0f64d97ba1
hamzaawais <hamzaawais@gmail.com>
2020-08-13 02:15:03 +0500

    Gallery added

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -3,6 +3,84 @@ import Header from '../Shared/Header'
 import Footer from '../Shared/Footer'
 import ChatBtn from '../Home/ChatBtn'
 import "../../assets/stylesheets/application.scss"
+import  * as Gallery1 from 'react-grid-gallery';
+
+// propTypes = {
+//   images: PropTypes.arrayOf(
+//       PropTypes.shape({
+//           src: PropTypes.string.isRequired,
+//           thumbnail: PropTypes.string.isRequired,
+//           srcset: PropTypes.array,
+//           caption: PropTypes.oneOfType([
+//               PropTypes.string,
+//               PropTypes.element
+//           ]),
+//           thumbnailWidth: PropTypes.number.isRequired,
+//           thumbnailHeight: PropTypes.number.isRequired
+//       })
+//   ).isRequired
+// };
+
+// defaultProps = {
+//   images: [
+//       {
+//           src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+//           thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
+//           thumbnailWidth: 320,
+//           thumbnailHeight: 174,
+//           caption: "After Rain (Jeshu John - designerspics.com)"
+//       },
+//       {
+//           src: "https://c6.staticflickr.com/9/8890/28897154101_a8f55be225_b.jpg",
+//           thumbnail: "https://c6.staticflickr.com/9/8890/28897154101_a8f55be225_n.jpg",
+//           thumbnailWidth: 320,
+//           thumbnailHeight: 183,
+//           caption: "37H (gratispgraphy.com)"
+//       },
+//       {
+//           src: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_b.jpg",
+//           thumbnail: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_n.jpg",
+//           thumbnailWidth: 271,
+//           thumbnailHeight: 320,
+//           caption: "Orange Macro (Tom Eversley - isorepublic.com)"
+//       },
+//       {
+//           src: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_b.jpg",
+//           thumbnail: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_n.jpg",
+//           thumbnailWidth: 320,
+//           thumbnailHeight: 213,
+//           caption: "201H (gratisography.com)"
+//       },
+//       {
+//           src: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg",
+//           thumbnail: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_n.jpg",
+//           thumbnailWidth: 320,
+//           thumbnailHeight: 213,
+//           caption: "Flower Interior Macro (Tom Eversley - isorepublic.com)"
+//       },
+//       {
+//           src: "https://c1.staticflickr.com/9/8707/28868704912_cba5c6600e_b.jpg",
+//           thumbnail: "https://c1.staticflickr.com/9/8707/28868704912_cba5c6600e_n.jpg",
+//           thumbnailWidth: 320,
+//           thumbnailHeight: 213,
+//           caption: "Man on BMX (Tom Eversley - isorepublic.com)"
+//       },
+//       {
+//           src: "https://c4.staticflickr.com/9/8578/28357117603_97a8233cf5_b.jpg",
+//           thumbnail: "https://c4.staticflickr.com/9/8578/28357117603_97a8233cf5_n.jpg",
+//           thumbnailWidth: 320,
+//           thumbnailHeight: 213,
+//           caption: "Ropeman - Thailand (Tom Eversley - isorepublic.com)"
+//       },
+//       {
+//           src: "https://c1.staticflickr.com/9/8056/28354485944_148d6a5fc1_b.jpg",
+//           thumbnail: "https://c1.staticflickr.com/9/8056/28354485944_148d6a5fc1_n.jpg",
+//           thumbnailWidth: 257,
+//           thumbnailHeight: 320,
+//           caption: "A photo by 贝莉儿 NG. (unsplash.com)"
+//       }
+//   ]
+// };
 
 function importAll(r) {
     let images = {};
@@ -11,6 +89,8 @@ function importAll(r) {
 }
 
 function Gallery(props) {
+    
+  
     const images = importAll(require.context('../../assets/images/', false, /\.(png|jpe?g)$/));
     const { t } = props;
     return (
@@ -145,7 +225,37 @@ function Gallery(props) {
               />
             </div>
             <div className="portrait genre">
+           
               <p> {t('gallery.genre_1')}</p>
+              <div className="h-gallery"> 
+          
+          <Gallery1
+              images={[
+                {
+                    src: images["bitmap-p-1@1x.png"],
+                    thumbnail: images["bitmap-p-1@1x.png"],
+                    thumbnailWidth: 320,
+                    thumbnailHeight: 174,
+                    caption: "After Rain (Jeshu John - designerspics.com)"
+                },
+                {
+                    src: images["bitmap-p-2@1x.png"],
+                    thumbnail: images["bitmap-p-2@1x.png"],
+                    thumbnailWidth: 320,
+                    thumbnailHeight: 183,
+                    caption: "37H (gratispgraphy.com)"
+                },
+                {
+                  src: images["bitmap-p-3@1x.png"],
+                  thumbnail: images["bitmap-p-3@1x.png"],
+                  thumbnailWidth: 320,
+                  thumbnailHeight: 183,
+                  caption: "37H (gratispgraphy.com)"
+              }
+              ]}
+              enableLightbox={true}
+              enableImageSelection={true}                         
+          /></div>
               <div className="grow pic pic-1">
                 <img alt="" src={images["bitmap-p-1@1x.png"]} />
               </div>
@@ -317,6 +427,31 @@ function Gallery(props) {
       </div>
     );
 }
+
+
+// function render () {
+//   return (
+//       <div style={{
+//           display: "block",
+//           minHeight: "1px",
+//           width: "100%",
+//           border: "1px solid #ddd",
+//           overflow: "auto"}}>
+//           <div style={{
+//                padding: "2px",
+//                color: "#666"
+//               }}>Current image: 0</div>
+//           <Gallery
+//               images={Gallery.defaultProps.images}
+//               enableLightbox={true}
+//               enableImageSelection={false}
+             
+
+              
+//           />
+//       </div>
+//   );
+// }
 
 
 export default Gallery
