f2f29793459d46d656af5f068222c75b5ddfc16e
Hamza Awais <hamza.awais@square63.org>
2020-08-15 01:41:27 +0500

    implemented lazy loading for home image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,35 +1,36 @@
-import React from 'react';
-import '../../assets/stylesheets/application.scss'
-import Header from '../Shared/Header'
-import HowItWords from './HowItWorks'
-import SuperEasy from './SuperEasy'
-import BestPrice from './BestPrice'
-import BestService from './BestService'
-import Footer from '../Shared/Footer'
-import ChatBtn from './ChatBtn'
-import drop17 from '../../assets/images/drop-17@1x.png'
+import React from "react";
+import "../../assets/stylesheets/application.scss";
+import Header from "../Shared/Header";
+import HowItWords from "./HowItWorks";
+import SuperEasy from "./SuperEasy";
+import BestPrice from "./BestPrice";
+import BestService from "./BestService";
+import Footer from "../Shared/Footer";
+import ChatBtn from "./ChatBtn";
+import drop17 from "../../assets/images/drop-17@1x.png";
+import { LazyLoadComponent } from "react-lazy-load-image-component";
 
-function Home(props){
-    const { t } = props;
-    return(
-        <div className="body">
-            <ChatBtn t= {t}/>
-            <div className="container-fluid">
-                <div id="hero" className="row">
-                    <div className="content" id="content">
-                    </div>
-                </div>
-                <Header t= {t} route='home'/>
-                <HowItWords t= {t}/>
-                <SuperEasy t= {t} />
-                <BestPrice t= {t}/>
-                <BestService t= {t}/>
-                <img src={drop17}  className='drop-17' alt="" />
-            </div>
-            <Footer t= {t}/>
-        </div>
-    );
+function Home(props) {
+  const { t } = props;
+  return (
+    <div className="body">
+      <ChatBtn t={t} />
+      <div className="container-fluid">
+        <LazyLoadComponent>
+          <div id="hero" className="row">
+            <div className="content" id="content"></div>
+          </div>
+        </LazyLoadComponent>
+        <Header t={t} route="home" />
+        <HowItWords t={t} />
+        <SuperEasy t={t} />
+        <BestPrice t={t} />
+        <BestService t={t} />
+        <img src={drop17} className="drop-17" alt="" />
+      </div>
+      <Footer t={t} />
+    </div>
+  );
 }
 
 export default Home;
-
--- src/components/Home/SuperEasy.js
+++ src/components/Home/SuperEasy.js
@@ -1,51 +1,55 @@
-import React from 'react';
-import SvgLeft from '../../assets/images/Left'
-import drop5 from '../../assets/images/drop-5@1x.png'
-import drop6 from '../../assets/images/drop-6@1x.png'
-import drop7 from '../../assets/images/drop-7@1x.png'
-import drop10 from '../../assets/images/drop-10@1x.png'
-import drop11 from '../../assets/images/drop-11@1x.png'
-import brush1 from '../../assets/images/brush-1@1x.png'
-import bitmap from '../../assets/images/bitmap_s_e@1x.png'
-import bitmapMedia from '../../assets/images/bitmap-media-1@1x.png'
-import '../../assets/stylesheets/application.scss'
+import React from "react";
+import SvgLeft from "../../assets/images/Left";
+import drop5 from "../../assets/images/drop-5@1x.png";
+import drop6 from "../../assets/images/drop-6@1x.png";
+import drop7 from "../../assets/images/drop-7@1x.png";
+import drop10 from "../../assets/images/drop-10@1x.png";
+import drop11 from "../../assets/images/drop-11@1x.png";
+import brush1 from "../../assets/images/brush-1@1x.png";
+import bitmap from "../../assets/images/bitmap_s_e@1x.png";
+import bitmapMedia from "../../assets/images/bitmap-media-1@1x.png";
+import "../../assets/stylesheets/application.scss";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
-
-function SuperEasy(props){
-    const { t } = props;
-    return(
-        <div id="super-easy" className="row ml0 mr0 p0">
-            <div className="drops">
-                <img src={drop5} className= 'drop-5' alt=""/>
-                <img src={drop6} className= 'drop-6' alt=""/>
-                <img src={drop7} className= 'drop-7' alt=""/>
-                <img src={drop10} className= 'drop-10' alt=""/>
-                <img src={drop11} className= 'drop-11' alt=""/>
-            </div>
-            <div className="col-sm-8 col-xs-12 offset-2 p0">
-                <div className="row">
-                    <div className="col-sm-9 col-xs-12 col-md-7">
-                        <img src={brush1} className= 'brush' alt=""/>
-                        <img src={bitmap} className= 's-e-img shadow-image' alt=""/>
-                        <img src={bitmapMedia} className= 's-e-img-media media shadow-image' alt=""/>
-                    </div>
-                    <div className="col-sm-5 col-xs-12 descriptions">
-                    <div className="title">
-                    {t('home_page.super_easy.title')}
-                    </div>
-                    {t('home_page.super_easy.description')}
-                    <p>
-                <a href="/prices">
-                {t('home_page.start_painting')}
-                    <SvgLeft className= "arrow m0"/>
-                </a>
+function SuperEasy(props) {
+  const { t } = props;
+  return (
+    <div id="super-easy" className="row ml0 mr0 p0">
+      <div className="drops">
+        <img src={drop5} className="drop-5" alt="" />
+        <img src={drop6} className="drop-6" alt="" />
+        <img src={drop7} className="drop-7" alt="" />
+        <img src={drop10} className="drop-10" alt="" />
+        <img src={drop11} className="drop-11" alt="" />
+      </div>
+      <div className="col-sm-8 col-xs-12 offset-2 p0">
+        <div className="row">
+          <div className="col-sm-9 col-xs-12 col-md-7">
+            <img src={brush1} className="brush" alt="" />
+            <LazyLoadImage
+              src={bitmap}
+              className="s-e-img shadow-image"
+              alt=""
+            />
+            <img
+              src={bitmapMedia}
+              className="s-e-img-media media shadow-image"
+              alt=""
+            />
+          </div>
+          <div className="col-sm-5 col-xs-12 descriptions">
+            <div className="title">{t("home_page.super_easy.title")}</div>
+            {t("home_page.super_easy.description")}
+            <p>
+              <a href="/prices">
+                {t("home_page.start_painting")}
+                <SvgLeft className="arrow m0" />
+              </a>
             </p>
-            </div>
-                </div>
-            </div>
+          </div>
         </div>
-
-    )
+      </div>
+    </div>
+  );
 }
-export default SuperEasy
-
+export default SuperEasy;
